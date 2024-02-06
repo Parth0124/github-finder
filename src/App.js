dbc2960fc144a36a787da7b74104f0ead13e0e65
Parth0124 <abhangparth@gmail.com>
2024-02-06 23:25:53 +0530

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
   return(
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
 
       <main>Content</main>
+      <Footer/>
     </div>
     </Router>
   );
--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,9 +3,26 @@ import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
-    <div>
-      Navbar
-    </div>
+    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
+        <div className='container mx-auto'>
+      <div className='FLWX-NONE PX-2 MX-2'>
+        <FaGithub className='inline pr-2 text-3xl'/>
+        <Link to='/' className='text-lg font-bold align-middle'>
+            Github Finder
+        </Link>
+      </div>
+      <div className='flex-1 px-2 mx-2'>
+        <div className='flex justify-end'>
+            <Link to='/' className='btn btn-ghost btn-sm rounded-btn'>
+                Home
+            </Link>
+            <Link to='/about' className='btn btn-ghost btn-sm rounded-btn'>
+                About
+            </Link>
+        </div>
+      </div>
+      </div>
+    </nav>
   )
 }
 
