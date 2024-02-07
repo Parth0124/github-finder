61022352511b24fbb6583ce0a23ec0892d4055a2
Parth0124 <abhangparth@gmail.com>
2024-02-07 07:32:47 +0530

    commit 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,8 @@
+import UserResults from "../components/users/UserResults"
+
 function Home() {
   return (
-    <div>
-      <h1 className="text-6xl">
-        Welcome
-      </h1>
-    </div>
+      <UserResults />
   )
 }
 
